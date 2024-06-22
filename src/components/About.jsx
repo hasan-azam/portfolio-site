348b3483c45c2ff0b8bbd4f290714f7b40fde478
Hasan <hasan1117@gmail.com>
2024-06-22 12:08:30 -0700

    Implemented beginnings of About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h1 className="my-20 text-center text-4xl">About <span className="text-neutral-500">Me</span></h1>
+    </div>
+  )
+}
+
+export default About
