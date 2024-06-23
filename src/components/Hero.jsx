c8e07f1a2cbf4bcdbbe4218bbcf9692f34c84623
Hasan <hasan1117@gmail.com>
2024-06-22 21:50:23 -0700

    Finished About section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
             Hasan Azam
         </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Hostinger Test</span>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
             <p className="my-2 max-width-xl py-6 font-light tracking-tighter">
                 {HERO_CONTENT}
             </p>
