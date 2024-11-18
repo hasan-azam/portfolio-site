a205fccb774c5a3ebf2c19a159ced6e1752e9513
Hasan <hasan1117@gmail.com>
2024-11-17 23:16:12 -0800

    Added Bluesky to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/hasanAzamLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaBluesky } from "react-icons/fa6";
 
 
 
@@ -12,6 +13,8 @@ const navbar = () => {
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
        <a href="https://www.linkedin.com/in/hasan-azam/" target="_blank"><FaLinkedin /></a> 
         <a href="https://github.com/hasan-azam" target="_blank"><FaGithub /></a>
+        <a href="https://bsky.app/profile/hasanazam.com" target="_blank"><FaBluesky /></a>
+
        
     </div>
   </nav>
